1c52e25272e517ae740416041ed1488f69465e5f
Hendryl-Almeida <dealmeidahendryl6@gmail.com>
2025-08-12 13:16:36 +0000

    acentos removidos

--- .github/trunfo.c
+++ .github/trunfo.c
@@ -3,14 +3,14 @@
     char Cidade1[30];
     char Codigocarta1[20];
     char Estado1[15];
-    float Área1, Pib1;
-    int pontos_turísticos1, População1;
+    float Area1, Pib1;
+    int pontos_turísticos1, Populaçao1;
     
     char Cidade2[30];
     char codigocarta2[20];
     char Estado2[15];
-    float Área2, Pib2;
-    int pontos_turísticos2, População2;
+    float Area2, Pib2;
+    int pontos_turísticos2, Populaçao2;
 
 printf("Insira a cidade: \n");
    scanf("%s", Cidade1);
@@ -21,11 +21,11 @@ scanf("%s", Codigocarta1);
 printf("Insira o Estado da cidade: \n");
 scanf("%s", Estado1);
 
-printf("Insira a População cidade: \n", População1);
-scanf("%d", &População1);
+printf("Insira a População cidade: \n", Populaçao1);
+scanf("%d", &Populaçao1);
 
-printf("Insira a Área da cidade:  \n", Área1);
-scanf("%f", &Área1);
+printf("Insira a Área da cidade:  \n", Area1);
+scanf("%f", &Area1);
 
 printf("insira o Pib da cidade: \n", Pib1);
 scanf("%f", &Pib1);
@@ -44,11 +44,11 @@ scanf("%s", Codigocarta2);
 printf("Insira o Estado da cidade: \n");
 scanf("%s", Estado2);
 
-printf("Insira a População cidade: \n", População2);
-scanf("%d", &População2);
+printf("Insira a População cidade: \n", Populaçao2);
+scanf("%d", &Populaçao2);
 
-printf("Insira a Área da cidade:  \n", Área2);
-scanf("%f", &Área2);
+printf("Insira a Área da cidade:  \n", Area2);
+scanf("%f", &Area2);
 
 printf("insira o Pib da cidade: \n", Pib2);
 scanf("%f", &Pib2);
