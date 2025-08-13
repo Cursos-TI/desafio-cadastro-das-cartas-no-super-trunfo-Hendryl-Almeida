39615ecd0c4cea0f2169bbb17cb1539ee4bfa049
Hendryl-Almeida <dealmeidahendryl6@gmail.com>
2025-08-13 12:42:44 +0000

    edição

--- .github/trunfo.c
+++ .github/trunfo.c
@@ -4,13 +4,13 @@
     char Codigocarta1[20];
     char Estado1[15];
     float Area1, Pib1;
-    int pontos_turisticos1, Populaçao1;
+    int pontos_turisticos1, Populacao1;
     
     char Cidade2[30];
     char Codigocarta2[20];
     char Estado2[15];
     float Area2, Pib2;
-    int pontos_turisticos2, Populaçao2;
+    int pontos_turisticos2, Populacao2;
 
 printf("Insira a cidade: \n");
    scanf("%29[^\n]", Cidade1);
@@ -58,7 +58,9 @@ printf("Insira a quantidade de pontos turísticos: \n");
 scanf("%d", &pontos_turisticos2);
    
 printf("\n");
-printf("\n--- Dados da Carta 1 ---\n"); printf("Cidade: %s\n", Cidade1); printf("Codigo: %s\n", Codigocarta1); printf("Estado: %s\n", Estado1); 
+printf("\n--- Dados da Carta 1 ---\n"); 
+printf("Cidade: %s\n", Cidade1); printf("Codigo: %s\n", Codigocarta1); 
+printf("Estado: %s\n", Estado1); 
 printf("Populacao: %d\n", Populaçao1); printf("Area: %.2f km²\n", Area1); 
 printf("PIB: %.2f\n", Pib1); printf("Pontos Turisticos: %d\n", pontos_turisticos1); 
 
